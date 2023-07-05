92f149f2ca7037d24456cc4df9b6973f8cfc8d0f
ahmed moussa <97238080+ahmed0moussa@users.noreply.github.com>
2023-07-05 17:12:49 +0100

    ajout list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   children:[
     {path:'',loadChildren:()=>import('./views/rh-views/dashboard/dashboard.module').then(m=>m.DashboardModule)},
     {path:'addcandidate',loadChildren:()=>import('./views/rh-views/add-candidate/add-candidate.module').then(m=>m.AddCandidateModule)},
-    {path:'listcandidate',loadChildren:()=>import('./views/common-views/list-candidate/list-candidate.module').then(m=>m.ListCandidateModule)}
+    {path:'listcandidate/:type',loadChildren:()=>import('./views/common-views/list-candidate/list-candidate.module').then(m=>m.ListCandidateModule)}
   ]
 },
   {path:'admin',component:AdminLayoutComponent,
@@ -17,7 +17,7 @@ const routes: Routes = [
     {path:'',loadChildren:()=>import('./views/admin-views/dashboard/dashboard.module').then(m=>m.DashboardModule)},
     {path:'adduserrh',loadChildren:()=>import('./views/admin-views/add-user-rh/add-user-rh.module').then(m=>m.AddUserRHModule)},
     {path:'listuserrh',loadChildren:()=>import('./views/admin-views/list-user-rh/list-user-rh.module').then(m=>m.ListUserRHModule)},
-    {path:'listcandidate',loadChildren:()=>import('./views/common-views/list-candidate/list-candidate.module').then(m=>m.ListCandidateModule)}
+    {path:'listcandidate/:type',loadChildren:()=>import('./views/common-views/list-candidate/list-candidate.module').then(m=>m.ListCandidateModule)}
   ]
   } 
   
--- src/app/views/common-views/list-candidate/list-candidate/list-candidate.component.ts
+++ src/app/views/common-views/list-candidate/list-candidate/list-candidate.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'datatables.net';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-list-candidate',
   templateUrl: './list-candidate.component.html',
   styleUrls: ['./list-candidate.component.css']
 })
-export class ListCandidateComponent {
+
+export class ListCandidateComponent implements AfterViewInit{
+  typePoste=''
+  constructor(private route:ActivatedRoute){
+    this.route.params.subscribe(data=>{
+      this.typePoste=data['type']
+    })
+  }
+  ngAfterViewInit(): void {
+    $(document).ready(function () {
+      $('#dataTable').DataTable();
+    });
+  }
 
 }
