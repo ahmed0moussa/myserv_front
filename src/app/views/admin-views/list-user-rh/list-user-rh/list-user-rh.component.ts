f8aafc73538596ed02dcaae86107d88faf91a322
ahmed moussa <97238080+ahmed0moussa@users.noreply.github.com>
2023-07-12 17:56:08 +0100

    i maked adduser

--- src/app/views/admin-views/list-user-rh/list-user-rh/list-user-rh.component.ts
+++ src/app/views/admin-views/list-user-rh/list-user-rh/list-user-rh.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-user-rh.component.css']
 })
 export class ListUserRhComponent {
+  password!: string;
+  showPassword: boolean = false;
+  data = [
+    { name: 'Sawssen HASSAYOUNE', email: 'Sawssen.hassayoune@myserv.com', password: 'azerty', isDisabled: true, showPassword: false },
+    { name: 'Hanine BADRI', email: 'hanine.badri@myserv.com', password: '', isDisabled: true, showPassword: false }
+  ];
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+  edit(item: any) {
+    item.isDisabled = false;
+  }
 
+  save(item: any) {
+    item.isDisabled = true;
+  }
+
+  delete(item: any) {
+    // Perform delete operation
+  }
 }
